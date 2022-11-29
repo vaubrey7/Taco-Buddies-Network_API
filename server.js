af91831aa4d0d75c7a4f2c7561c37ed923897938
Vaughn Aubrey <vaubrey7@gmail.com>
2022-11-28 21:48:08 -0700

    server.js fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongo://localhost:'
+)
 
 db.once('open', () => {
   app.listen(PORT, () => {
