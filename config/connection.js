71971be7e2781ec3d1e5d091d91ec095dd6a5002
Vaughn Aubrey <vaubrey7@gmail.com>
2022-11-28 18:08:41 -0700

    addedconfig

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// mongoose.set('debut', true);
+
+module.exports = connection;
