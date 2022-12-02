97be53e9bd4726124c5ce218517ab1a02aeaa888
Vaughn Aubrey <vaubrey7@gmail.com>
2022-12-02 11:51:54 -0700

    updates

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+// this is our port set to 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
+// the console log will tell us if the port is succesfull in running
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
